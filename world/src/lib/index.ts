a54741e12dc0ddf24a3873c0bc9f958305d08589
Metal Finger <dev.prajwalp@gmail.com>
2025-12-16 16:01:10 +0530

    empty functions

--- world/src/lib/index.ts
+++ world/src/lib/index.ts
@@ -1,1 +1,39 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+import * as THREE from 'three';
+
+export function initScene(canvas: HTMLCanvasElement) {
+	const scene = new THREE.Scene();
+
+	const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+	const renderer = new THREE.WebGLRenderer({ canvas });
+
+	renderer.setPixelRatio(window.devicePixelRatio);
+	const resize = () => {
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+	};
+	resize();
+
+	camera.position.setZ(30);
+	camera.position.setX(-3);
+
+	const pointLight = new THREE.PointLight(0xffffff);
+
+	scene.add(pointLight);
+	scene.background = new THREE.TextureLoader().load('space.jpg');
+
+	window.addEventListener('resize', resize);
+
+	function start() {}
+
+	function stop() {}
+
+	function dispose() {
+		renderer.dispose();
+	}
+
+	return { start, stop, dispose };
+}
