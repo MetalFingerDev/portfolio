483319a36b6ca9133d48c4ef413af10f51da6f80
Metal Finger <dev.prajwalp@gmail.com>
2026-01-15 13:42:39 +0530

    fix: correct import paths for CelestialBody in Planet and Star classes

--- void3/src/planetary/Planet.ts
+++ void3/src/planetary/Planet.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { type CelestialBody } from "./regions/Region";
+import { type CelestialBody } from "../regions/Region";
 
 /**
  * Planet with a visible Mesh
--- void3/src/stellar/Star.ts
+++ void3/src/stellar/Star.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { type CelestialBody } from "./Region";
+import { type CelestialBody } from "../regions/Region";
 
 /**
  * Simple Star with a visible Mesh and an optional Light
